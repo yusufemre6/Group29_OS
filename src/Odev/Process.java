2a625da1e473af61c24769144448a6b5c801d0c7
yusufemre6 <kahmetf@gmail.com>
2023-12-31 22:37:45 +0300

    Process oluşturuldu.

---
+++ src/Odev/Process.java
@@ -0,0 +1,19 @@
+package Odev;
+
+public class Process {
+
+	public int id;
+	public int arriveTime;
+	public int queueTime; 
+	public int workTime; 
+	public int priority;
+	public int burstTime;
+	public int memorySize;
+	public int printers;
+	public int scanner;
+	public int modem;
+	public int cdDrivers;
+	public String status;
+	public int begginingAddress;
+
+}
